d9f7de366486b36a2097f310ddae2be3c47c41e7
Patrick Boivin <pboivin@gmail.com>
2023-08-29 11:30:52 -0400

    FE - add @astrojs/alpinejs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind({
-            applyBaseStyles: false,
-        }),
-    ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), alpinejs()]
 });
