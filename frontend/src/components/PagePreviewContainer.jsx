dda064427fabd10aa18b1c5e2fe400ac35eb836e
Patrick Boivin <pboivin@gmail.com>
2023-09-02 13:02:16 -0400

    FE - add PageContent

---
+++ frontend/src/components/PagePreviewContainer.jsx
@@ -0,0 +1,18 @@
+import { h, Fragment } from 'preact';
+import { getPreview } from '../api/preview.js';
+
+import PageContent from './PageContent.jsx';
+
+function getToken() {
+    const params = new URLSearchParams(document.location.search);
+
+    return params.get('token');
+}
+
+const token = getToken();
+
+const previewData = token ? await getPreview(token) : null;
+
+export default function PagePreviewContainer() {
+    return <>{!previewData ? <div class="p-8 text-center text-red-500">Not available</div> : <PageContent page={previewData.page} />}</>;
+}
