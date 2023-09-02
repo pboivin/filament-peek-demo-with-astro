e9721db4e0def5580d9c917ec02d7aebff673203
Patrick Boivin <pboivin@gmail.com>
2023-09-02 13:11:21 -0400

    FE - lint

--- frontend/src/api/menus.js
+++ frontend/src/api/menus.js
@@ -3,7 +3,6 @@ import { API_BASE } from '../constants';
 export async function getMenus() {
     const response = await fetch(`${API_BASE}/content/menus`);
     const data = await response.json();
-
     const menus = {};
 
     for (let menu of data) {
--- frontend/src/api/preview.js
+++ frontend/src/api/preview.js
@@ -1,9 +1,9 @@
 import { API_BASE } from '../constants';
 
 export async function getPreview(token) {
-    const response = await fetch(`${API_BASE}/preview/${token}`);
-
     try {
+        const response = await fetch(`${API_BASE}/preview/${token}`);
+
         return (await response.json()) || null;
     } catch (e) {
         return null;
