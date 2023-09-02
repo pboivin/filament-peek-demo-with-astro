5f441a089bfbb845e6d8423c6370f81cca8625fd
Patrick Boivin <pboivin@gmail.com>
2023-09-02 17:13:29 -0400

    FE - render post content blocks

---
+++ frontend/src/components/blocks/PageCard.jsx
@@ -0,0 +1,5 @@
+import { h, Fragment } from 'preact';
+
+export default function PageCard({ data }) {
+    return '_page_card_';
+}
---
+++ frontend/src/components/blocks/PostCard.jsx
@@ -0,0 +1,5 @@
+import { h, Fragment } from 'preact';
+
+export default function PostCard({ data }) {
+    return '_post_card_';
+}
