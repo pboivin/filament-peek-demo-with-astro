ca2c06acf4778b842aca79b6298c9111f7c3794c
Patrick Boivin <pboivin@gmail.com>
2023-09-02 12:09:35 -0400

    FE - add preact

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import alpinejs from "@astrojs/alpinejs";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
-  }), alpinejs()]
+  }), alpinejs(), preact()]
 });
