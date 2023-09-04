6a119a539c60dfbc1a052038bdac93418267664d
Patrick Boivin <pboivin@gmail.com>
2023-09-04 10:57:39 -0400

    FE - fix card blocks

--- frontend/src/components/blocks/PageCard.jsx
+++ frontend/src/components/blocks/PageCard.jsx
@@ -1,9 +1,12 @@
 import { h, Fragment } from 'preact';
 
 export default function PageCard({ data }) {
+    const text = data.text || data.page?.title;
+    const url = `/${data.page?.slug}`;
+
     return (
-        <a class="relative aspect-[4/3] p-4 border bg-gray-100" href={data.card_url}>
-            {data.card_text}
+        <a class="relative aspect-[4/3] p-4 border bg-gray-100" href={url}>
+            {text}
         </a>
     );
 }
