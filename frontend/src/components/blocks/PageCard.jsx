a233884758f1f5f0d5c4443aefa998b26d338d5b
Patrick Boivin <pboivin@gmail.com>
2023-09-04 10:44:20 -0400

    FE - fix PostCard position

--- frontend/src/components/blocks/PageCard.jsx
+++ frontend/src/components/blocks/PageCard.jsx
@@ -2,7 +2,7 @@ import { h, Fragment } from 'preact';
 
 export default function PageCard({ data }) {
     return (
-        <a class="aspect-[4/3] p-4 border bg-gray-100" href={data.card_url}>
+        <a class="relative aspect-[4/3] p-4 border bg-gray-100" href={data.card_url}>
             {data.card_text}
         </a>
     );
--- frontend/src/components/blocks/PostCard.jsx
+++ frontend/src/components/blocks/PostCard.jsx
@@ -2,7 +2,7 @@ import { h, Fragment } from 'preact';
 
 export default function PostCard({ data }) {
     return (
-        <a class="aspect-[4/3] p-4 border bg-gray-100" href={data.card_url}>
+        <a class="relative aspect-[4/3] p-4 border bg-gray-100" href={data.card_url}>
             <img
                 class="absolute top-0 left-0 w-full h-full p-0 m-0 opacity-20 object-center object-cover"
                 src={data.card_image}
