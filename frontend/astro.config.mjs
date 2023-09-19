85f496499fedaa0619d88aac38994af2879bbcde
Patrick Boivin <pboivin@gmail.com>
2023-09-19 09:56:58 -0400

    FE - update astro config

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    server: {
+        host: true,
+        port: 3000,
+    },
+});
