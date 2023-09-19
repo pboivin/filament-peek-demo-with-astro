46aa2af957fdf45180d02904950ecfa4e2e0fd58
Patrick Boivin <pboivin@gmail.com>
2023-09-19 10:04:32 -0400

    FE - setup tailwind and preact

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        host: true,
-        port: 3000,
-    },
+  server: {
+    host: true,
+    port: 3000
+  },
+  integrations: [tailwind(), preact()]
 });
